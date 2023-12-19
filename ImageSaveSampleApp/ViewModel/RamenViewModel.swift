402dd52f93aef909bdf166334b7d9190beeffed5
RikutoSato <satorikuto1064@gmail.com>
2023-12-19 15:15:08 +0900

    関数の上にコメントをする場合は、///にする。

--- ImageSaveSampleApp/ViewModel/RamenViewModel.swift
+++ ImageSaveSampleApp/ViewModel/RamenViewModel.swift
@@ -11,48 +11,46 @@ class RamenViewModel: ObservableObject {
     @Published private var model = RamenModel()
 
     // MARK: ここでプロパティを連携させる
-    // ラーメンのリストをビューに提供するプロパティ
-    var ramens: [Ramen] {
-        model.ramens
-    }
+    /// ラーメンのリストをビューに提供するプロパティ
+    var ramens: [Ramen] { model.ramens }
     
     var uploadImageButtonDisabled: Bool {
         return selectedImageData == nil
     }
 
-    // ラーメン追加ビューの表示状態を管理するプロパティ
+    /// ラーメン追加ビューの表示状態を管理するプロパティ
     var isPresentingAddView: Bool {
         get { model.isPresentingAddView }
         set { model.isPresentingAddView = newValue }
     }
 
-    // 画像選択ピッカーの表示状態を管理するプロパティ
+    /// 画像選択ピッカーの表示状態を管理するプロパティ
     var showImagePicker: Bool {
         get { model.showImagePicker }
         set { model.showImagePicker = newValue }
     }
 
-    // 選択された画像データにアクセスするプロパティ
+    /// 選択された画像データにアクセスするプロパティ
     var selectedImageData: Data? {
         get { model.selectedImageData }
         set { model.selectedImageData = newValue }
     }
 
-    // アップロード完了の状態をビューに提供するプロパティ
+    /// アップロード完了の状態をビューに提供するプロパティ
     var isUploadCompleted: Bool {
         get { model.isUploadCompleted }
         set { model.isUploadCompleted = newValue }
     }
 
-    // アップロードされた時のアラートを出す変数
+    /// アップロードされた時のアラートを出す変数
     var showAlert: Bool {
         get { model.showAlert }
         set { model.showAlert = newValue }
     }
 
-    // 選択された画像をFirebase Storageにアップロードするメソッド
-    // 画像アップロードのメソッド（更新）
-    // 画像アップロードのメソッド
+    /// 選択された画像をFirebase Storageにアップロードするメソッド
+    /// 画像アップロードのメソッド（更新）
+    /// 画像アップロードのメソッド
     func uploadImage() {
         guard let imageData = selectedImageData else { return }
 
@@ -70,28 +68,28 @@ class RamenViewModel: ObservableObject {
     }
 
 
-    // アップロードされた画像URLを含む新しいRamenインスタンスを追加するメソッド
+    /// アップロードされた画像URLを含む新しいRamenインスタンスを追加するメソッド
     private func addRamenWithImage(url: String) {
         // ここで 'name' と 'shop' に実際の値を設定する
         addRamen(name: "New Ramen", shop: "Ramen Shop", rating: 5, imageUrl: url)
     }
 
-    // アップロードやその他の処理で発生したエラーを処理するメソッド
+    /// アップロードやその他の処理で発生したエラーを処理するメソッド
     private func handleError(_ error: Error) {
         // ここにエラー処理のロジックを実装
     }
 
-    // 新しいRamenインスタンスを追加するメソッド
+    /// 新しいRamenインスタンスを追加するメソッド
     func addRamen(name: String, shop: String, rating: Int, imageUrl: String? = nil) {
         model.addRamen(name: name, shop: shop, rating: rating, imageUrl: imageUrl)
     }
 
-    // 特定のRamenインスタンスをリストから削除するメソッド
+    /// 特定のRamenインスタンスをリストから削除するメソッド
     func removeRamen(at offsets: IndexSet) {
         model.removeRamens(at: offsets)
     }
 
-    // ラーメン追加ビューの表示状態を切り替えるメソッド
+    /// ラーメン追加ビューの表示状態を切り替えるメソッド
     func toggleAddRamenView() {
         model.toggleAddRamenView()
     }
