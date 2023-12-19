4300ad410e5f1e74de35b372e1b9ea36d2c6ac33
RikutoSato <satorikuto1064@gmail.com>
2023-12-19 15:16:03 +0900

    ファイル内で読んでいる関数はprivateをつける

--- ImageSaveSampleApp/ViewModel/RamenViewModel.swift
+++ ImageSaveSampleApp/ViewModel/RamenViewModel.swift
@@ -10,7 +10,6 @@ import SwiftUI
 class RamenViewModel: ObservableObject {
     @Published private var model = RamenModel()
 
-    // MARK: ここでプロパティを連携させる
     /// ラーメンのリストをビューに提供するプロパティ
     var ramens: [Ramen] { model.ramens }
     
@@ -67,7 +66,6 @@ class RamenViewModel: ObservableObject {
         }
     }
 
-
     /// アップロードされた画像URLを含む新しいRamenインスタンスを追加するメソッド
     private func addRamenWithImage(url: String) {
         // ここで 'name' と 'shop' に実際の値を設定する
@@ -80,12 +78,12 @@ class RamenViewModel: ObservableObject {
     }
 
     /// 新しいRamenインスタンスを追加するメソッド
-    func addRamen(name: String, shop: String, rating: Int, imageUrl: String? = nil) {
+    private func addRamen(name: String, shop: String, rating: Int, imageUrl: String? = nil) {
         model.addRamen(name: name, shop: shop, rating: rating, imageUrl: imageUrl)
     }
 
     /// 特定のRamenインスタンスをリストから削除するメソッド
-    func removeRamen(at offsets: IndexSet) {
+    private func removeRamen(at offsets: IndexSet) {
         model.removeRamens(at: offsets)
     }
 
