53f8aa7afdc2352ca89b8e6eede54016968201d3
yuruhuwakumiai <yuruhuwakumiai@gmail.com>
2024-01-01 15:48:23 +0900

    Modelの関数のコメントを///に修正

--- ImageSaveSampleApp/Model/RamenModel.swift
+++ ImageSaveSampleApp/Model/RamenModel.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import FirebaseStorage
 
 struct RamenModel {
     // firebaseをインスタンスしてリポジトリを使用する
@@ -23,25 +22,25 @@ struct RamenModel {
     var showAlert = false
 
     // MARK: 関数はここに追加
-    // Firebase Storageに画像データをアップロードし、URLを取得する
+    /// Firebase Storageに画像データをアップロードし、URLを取得する
     mutating func uploadImage(_ imageData: Data, completion: @escaping (Result<String, Error>) -> Void) {
         firebaseStorageRepository.uploadImageData(imageData, completion: completion)
     }
 
-    // 新しいラーメンのインスタンスを作成し、リストに追加する
+    /// 新しいラーメンのインスタンスを作成し、リストに追加する
     mutating func addRamen(name: String, shop: String, rating: Int, imageUrl: String? = nil) {
         let newRamen = Ramen(name: name, shop: shop, rating: rating, imageUrl: imageUrl)
         ramens.append(newRamen)
     }
 
     // リストから特定のラーメンを削除する
-    // IndexSetを使って複数の要素を削除することも可能
+    /// IndexSetを使って複数の要素を削除することも可能
     mutating func removeRamens(at offsets: IndexSet) {
         ramens.remove(atOffsets: offsets)
     }
 
     // ラーメン追加ビューの表示状態を切り替える
-    // isPresentingAddViewの状態をトグルする（true⇔false）
+    /// isPresentingAddViewの状態をトグルする（true⇔false）
     mutating func toggleAddRamenView() {
         isPresentingAddView.toggle()
     }
